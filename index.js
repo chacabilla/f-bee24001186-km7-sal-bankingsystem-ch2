13f02e0c043288e716512aba1ee48ce63c3a7f8d
chacabilla <salsabilla.aulia.tik22@mhsw.pnj.ac.id>
2024-11-19 21:14:18 +0700

    feat: add sentry

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
+require('./src/libs/sentry.js');
 const express = require('express');
+const Sentry = require('@sentry/node');
 const bodyParser = require('body-parser');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
@@ -15,18 +17,19 @@ app.use(bodyParser.json());
 // media handler
 app.use('/images', express.static('uploads/images'));
 
-const mediaRoutes = require('./src/routes/media');
-app.use('/api/v1/media', mediaRoutes);
-
 // routes
+const emailRoutes = require('./src/routes/email');
 const userRoutes = require('./src/routes/users');
 const accountRoutes = require('./src/routes/accounts');
 const transactionRoutes = require('./src/routes/transactions');
+const mediaRoutes = require('./src/routes/media');
 
 // use routes
+app.use('/api/v1/email', emailRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/accounts', accountRoutes);
 app.use('/api/v1/transactions', transactionRoutes);
+app.use('/api/v1/media', mediaRoutes);
 
 // swagger routes
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -34,6 +37,13 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get('/', (req, res) => {
     res.render('index'); // Render index.ejs
 });
+
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error(500, "My awesome Sentry error!");
+  });
+  
+Sentry.setupExpressErrorHandler(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
